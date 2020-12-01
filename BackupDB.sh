2b5fd3aff70c4ce7214bfd8aeb93848a4e1f86ed
NekoMeowy <60037266+NekoMeowy@users.noreply.github.com>
2020-12-01 14:55:38 +0700

    Create BackupDB.sh

---
+++ BackupDB.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+USERNAME="root"
+PASSWORD="root"
+SERVER="localhost"
+ 
+# local directory to pickup Sql file
+FILE="/.Files/Database/"
+ 
+# remote server directory to upload backup
+BACKUPDIR="AI/Database/"
+ 
+# login to remote server
+ftp -n -i $SERVER <<EOF
+user $USERNAME $PASSWORD
+cd $BACKUPDIR
+mput $FILE/*.sql
+quit
+EOF
